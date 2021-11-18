ceedfee78ce4659aa70c7231b9447a882d650004
Kim Black <kimblackaz@gmail.com>
2021-11-18 12:51:42 -0700

    added isAuth + delete route

--- routes/result.js
+++ routes/result.js
@@ -6,15 +6,15 @@ const isAuth = require('../middleware/is-auth');
 const controller = require('../controllers/result')
 
 //save one set of results
-// router.post('/saveResult', isAuth, controller.postSave);
-router.post('/saveResult', controller.postSave);
+router.post('/saveResult', isAuth, controller.postSave);
+// router.post('/saveResult', controller.postSave);
 
 //delete one set of results
-// router.post('/deleteResult', isAuth, controller.postDelete);
-router.post('/deleteResult', controller.postDelete);
+router.delete('/deleteResult', isAuth, controller.postDelete);
+// router.post('/deleteResult', controller.postDelete);
 
 //get all saved results
-// router.get('/getResults', isAuth, controller.getResults);
-router.get('/getResults', controller.getResults);
+router.get('/getResults', isAuth, controller.getResults);
+// router.get('/getResults', controller.getResults);
 
 module.exports = router;
