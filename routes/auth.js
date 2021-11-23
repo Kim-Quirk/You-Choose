01330cfa6e0d495a43523ea62054df5f06419c58
Kim Black <kimblackaz@gmail.com>
2021-11-23 10:58:53 -0700

    Added back in confirm password stuff

--- routes/auth.js
+++ routes/auth.js
@@ -44,6 +44,12 @@ router.post(
         .isLength({ min: 5 })
         .isAlphanumeric()
         .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords have to match!');
+            }
+            return true;
+        })
     ],
      controller.postSignup);
 
